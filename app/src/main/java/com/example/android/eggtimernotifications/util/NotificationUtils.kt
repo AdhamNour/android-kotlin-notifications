f3d6805ad533da5970694c5fe11b56d7f058568f
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-10-21 12:49:43 +0200

    14

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -53,7 +53,13 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.0 add style
 
     // TODO: Step 2.2 add snooze action
-
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        PendingIntent.FLAG_ONE_SHOT
+    )
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
     val builder = NotificationCompat.Builder(
@@ -72,7 +78,11 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.1 add style to builder
 
     // TODO: Step 2.3 add snooze action
-
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
     // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
